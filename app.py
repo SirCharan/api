c193ca397c556a325aa64b67c58ca565dfb3a5b6
Charandeep Kapoor <48174619+SirCharan@users.noreply.github.com>
2024-02-20 18:45:44 +0530

    changes

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ def get_balances():
         return jsonify({'available_balance': balances['available_balance']})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+    
+@app.route("/")
+def hello():
+    return "hello world", 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv('PORT', 5000), host='0.0.0.0')
